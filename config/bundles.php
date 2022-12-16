55f2433a91faa4a22345201c9776229d88b30e3e
FERRALI-Mario <you@example.com>
2022-12-16 14:51:11 +0100

    feat: ajout du bundle make

--- config/bundles.php
+++ config/bundles.php
@@ -1,17 +1,9 @@
 <?php
 
-declare(strict_types = 1);
-
-/*
- * This file is part of the Stello API project.
- * (c) Stello <dev@stello.eu>
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class            => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class             => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class     => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
