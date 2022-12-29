b7108578fa51d81f544a47747c9c04f7159ad36f
tom-guastapaglia <tomguasta@yahoo.fr>
2022-12-29 11:38:50 +0100

    add webprofiler toolbar

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Knp\DoctrineBehaviors\DoctrineBehaviorsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
