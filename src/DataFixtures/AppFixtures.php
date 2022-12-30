db38d5b53d6485b9a06567a5e3f1cae6273e21cc
tom-guastapaglia <tomguasta@yahoo.fr>
2022-12-30 15:52:35 +0100

    Fixtures Users

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,15 +11,45 @@ declare(strict_types = 1);
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 class AppFixtures extends Fixture
 {
+    private Generator $faker;
+
+    /**
+     * Init Fixture Class.
+     */
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Create customer
+        $userCustomer = new User();
+        $userCustomer->setEmail($this->faker->email())
+                      ->setUsername($this->faker->userName())
+                      ->setName($this->faker->name())
+                      ->setRoles(['ROLE_USER'])
+                      ->setLastname($this->faker->lastName())
+                      ->setPassword('$2y$13$Avfr0GAnTYFWtBdm7lOi3eiZK0.frdZ4hjV2aBAu7gfdg2QFLy.EK');
+        $manager->persist($userCustomer);
+
+        // Create admin account
+        $userAdmin = new User();
+        $userAdmin->setEmail('admin@moneyes.fr')
+                   ->setUsername('moneyes')
+                   ->setName('Moneyes')
+                   ->setLastname('Invest')
+                   ->setPassword('$2y$13$Q7K9ZktfHj9n.QelyHjauuZkeneJlCUJvVcPPbq3GsdPYYcarY53G')
+                   ->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
+        $manager->persist($userAdmin);
 
         $manager->flush();
     }
