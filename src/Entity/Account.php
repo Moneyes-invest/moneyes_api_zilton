eb049b161b5f58349a714dd20c01c0f289bc5707
tom-guastapaglia <tomguasta@yahoo.fr>
2023-01-04 22:04:56 +0100

    crud Accounts

--- src/Entity/Account.php
+++ src/Entity/Account.php
@@ -11,33 +11,63 @@ declare(strict_types = 1);
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\AccountRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
 use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Uid\Uuid;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 #[ORM\Entity(repositoryClass: AccountRepository::class)]
+#[ApiResource(
+	operations: [
+		new GetCollection(normalizationContext: ['groups' => ['get:accounts']]),
+		new Get(normalizationContext: ['groups' => ['get:accounts', 'get:account']]),
+		new Post(),
+		new Put(),
+		new Patch(),
+		new Delete(),
+	],
+	denormalizationContext: ['groups' => ['create:account', 'update:account']],
+)]
 class Account
 {
     #[ORM\Id]
     #[ORM\Column(type: UuidType::NAME, unique: true)]
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    #[Groups(['get:account', 'get:accounts'])]
     private Uuid $id;
 
     #[ORM\ManyToOne]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['get:account', 'create:account'])]
+    #[Assert\NotBlank(groups: ['create:account'])]
     private ?Exchange $idExchange = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['get:account', 'create:account'])]
+    #[Assert\NotBlank(groups: ['create:account'])]
     private ?string $privateKey = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['get:account', 'create:account'])]
+    #[Assert\NotBlank(groups: ['create:account'])]
     private ?string $publicKey = null;
 
     #[ORM\ManyToOne(inversedBy: 'account')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['get:account', 'create:account'])]
+    #[Assert\NotBlank(groups: ['create:account'])]
     private ?User $idUser = null;
 
     public function getId(): ?Uuid
