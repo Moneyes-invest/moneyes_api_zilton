a2451f542964282bf95e20b8144d28ccd0754553
tom-guastapaglia <tomguasta@yahoo.fr>
2023-01-04 21:58:29 +0100

    Crud exchange

--- src/Entity/Exchange.php
+++ src/Entity/Exchange.php
@@ -12,15 +12,32 @@ declare(strict_types = 1);
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\ExchangeRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
 use Symfony\Bridge\Doctrine\Types\UuidType;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Uid\Uuid;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ExchangeRepository::class)]
-#[ApiResource()]
+#[ApiResource(
+	operations: [
+		new GetCollection(normalizationContext: ['groups' => ['get:exchanges']]),
+		new Get(normalizationContext: ['groups' => ['get:exchanges', 'get:exchange']]),
+		new Post(),
+		new Put(),
+		new Patch(),
+		new Delete(),
+	],
+	denormalizationContext: ['groups' => ['create:exchange', 'update:exchange']],
+)]
 class Exchange
 {
     #[ORM\Id]
@@ -31,6 +48,8 @@ class Exchange
     private Uuid $id;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(groups: ['create:exchange'])]
+    #[Groups(['create:exchange'])]
     private string $label;
 
     public function __toString(): string
