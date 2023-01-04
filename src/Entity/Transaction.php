16120d93dc4279818394fa2bc9b402df713b7d88
tom-guastapaglia <tomguasta@yahoo.fr>
2023-01-04 18:27:01 +0100

    GET GETCOLLECTION Transactions

--- src/Entity/Transaction.php
+++ src/Entity/Transaction.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare( strict_types=1 );
 
 /*
  * This file is part of the Moneyes API project.
@@ -12,6 +12,12 @@ declare(strict_types = 1);
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\TransactionRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
@@ -19,163 +25,163 @@ use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
 use Symfony\Bridge\Doctrine\Types\UuidType;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Uid\Uuid;
+use Symfony\Component\Validator\Constraints as Assert;
 
-#[ORM\Entity(repositoryClass: TransactionRepository::class)]
-#[ApiResource()]
-class Transaction
-{
-    #[ORM\Id]
-    #[ORM\Column(type: UuidType::NAME, unique: true)]
-    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
-    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
-    #[Groups(['get:transaction', 'get:transactions'])]
-    private Uuid $id;
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-    #[Groups('get:transaction')]
-    private ?\DateTimeInterface $date = null;
+#[ORM\Entity( repositoryClass: TransactionRepository::class )]
+#[ApiResource(
+	operations: [
+		new GetCollection( normalizationContext: [ 'groups' => [ 'get:transactions' ] ] ),
+		new Get( normalizationContext: [ 'groups' => [ 'get:transactions', 'get:transaction' ] ] ),
+		new Post(),
+		new Put(),
+		new Patch(),
+		new Delete(),
+	],
+	denormalizationContext: ['groups' => ['create:transaction', 'update:transaction']],
+)]
+class Transaction {
+	#[ORM\Id]
+	#[ORM\Column( type: UuidType::NAME, unique: true )]
+	#[ORM\GeneratedValue( strategy: 'CUSTOM' )]
+	#[ORM\CustomIdGenerator( class: UuidGenerator::class )]
+	#[Groups( [ 'get:transaction', 'get:transactions' ] )]
+	private Uuid $id;
 
-    #[ORM\Column]
-    #[Groups(['get:transaction'])]
-    private ?float $value = null;
+	#[ORM\Column( type: Types::DATETIME_MUTABLE )]
+	#[Groups( 'get:transaction', 'create:transaction')]
+	#[Assert\NotBlank( groups: [ 'create:transaction' ] )]
+	private ?\DateTimeInterface $date = null;
 
-    #[ORM\ManyToOne]
-    #[ORM\JoinColumn(nullable: false)]
-    #[Groups(['get:transaction'])]
-    private ?Exchange $idExchange = null;
+	#[ORM\Column]
+	#[Groups( [ 'get:transaction', 'create:transaction' ] )]
+	#[Assert\NotBlank( groups: [ 'create:transaction' ] )]
+	private ?float $value = null;
 
-    #[ORM\ManyToOne(inversedBy: 'transactions')]
-    #[ORM\JoinColumn(nullable: false)]
-    #[Groups(['get:transaction'])]
-    private ?User $idUser = null;
+	#[ORM\ManyToOne]
+	#[ORM\JoinColumn( nullable: false )]
+	#[Groups( [ 'get:transaction', 'create:transaction' ] )]
+	private ?Exchange $idExchange = null;
 
-    #[ORM\Column(length: 255)]
-    #[Groups(['get:transaction'])]
-    private ?string $type = null;
+	#[ORM\ManyToOne( inversedBy: 'transactions' )]
+	#[ORM\JoinColumn( nullable: false )]
+	#[Groups( [ 'get:transaction', 'create:transaction' ] )]
+	#[Assert\NotBlank( groups: [ 'create:transaction' ] )]
+	private ?User $idUser = null;
 
-    #[ORM\ManyToOne]
-    #[ORM\JoinColumn(nullable: false)]
-    #[Groups(['get:transaction'])]
-    private ?Currency $idCurrency = null;
+	#[ORM\Column( length: 255 )]
+	#[Groups( [ 'get:transaction' ] )]
+	private ?string $type = null;
 
-    #[ORM\Column(length: 255)]
-    #[Groups(['get:transaction'])]
-    private string $orderDirection = '';
+	#[ORM\ManyToOne]
+	#[ORM\JoinColumn( nullable: false )]
+	#[Groups( [ 'get:transaction', 'create:transaction' ] )]
+	#[Assert\NotBlank( groups: [ 'create:transaction' ] )]
+	private ?Currency $idCurrency = null;
 
-    #[ORM\Column]
-    #[Groups(['get:transaction'])]
-    private int $amount;
+	#[ORM\Column( length: 255 )]
+	#[Groups( [ 'get:transaction', 'create:transaction' ] )]
+	#[Assert\NotBlank( groups: [ 'create:transaction' ] )]
+	private string $orderDirection = '';
 
-    public function getId(): ?Uuid
-    {
-        return $this->id;
-    }
+	#[ORM\Column]
+	#[Groups( [ 'get:transaction', 'create:transaction' ] )]
+	#[Assert\NotBlank( groups: [ 'create:transaction' ] )]
+	private int $amount;
 
-    public function getDate(): ?\DateTimeInterface
-    {
-        return $this->date;
-    }
+	public function getId(): ?Uuid {
+		return $this->id;
+	}
 
-    public function setDate(\DateTimeInterface $date): self
-    {
-        $this->date = $date;
+	public function getDate(): ?\DateTimeInterface {
+		return $this->date;
+	}
 
-        return $this;
-    }
+	public function setDate( \DateTimeInterface $date ): self {
+		$this->date = $date;
 
-    public function getValue(): ?float
-    {
-        return $this->value;
-    }
+		return $this;
+	}
 
-    public function setValue(float $value): self
-    {
-        $this->value = $value;
+	public function getValue(): ?float {
+		return $this->value;
+	}
 
-        return $this;
-    }
+	public function setValue( float $value ): self {
+		$this->value = $value;
 
-    public function getIdExchange(): ?Exchange
-    {
-        return $this->idExchange;
-    }
+		return $this;
+	}
 
-    public function setIdExchange(?Exchange $idExchange): self
-    {
-        $this->idExchange = $idExchange;
+	public function getIdExchange(): ?Exchange {
+		return $this->idExchange;
+	}
 
-        return $this;
-    }
+	public function setIdExchange( ?Exchange $idExchange ): self {
+		$this->idExchange = $idExchange;
 
-    public function getIdUser(): ?User
-    {
-        return $this->idUser;
-    }
+		return $this;
+	}
 
-    public function setIdUser(?User $idUser): self
-    {
-        $this->idUser = $idUser;
+	public function getIdUser(): ?User {
+		return $this->idUser;
+	}
 
-        return $this;
-    }
+	public function setIdUser( ?User $idUser ): self {
+		$this->idUser = $idUser;
 
-    public function getType(): ?string
-    {
-        return $this->type;
-    }
+		return $this;
+	}
 
-    /**
-     * Specify transaction type (Spot, Futures, Margin, etc.) Type is different between exchanges and currency.
-     *
-     * @param string $type (default = "SPOT" for 0.0.1-pre-alpha)
-     *
-     * @return $this
-     */
-    public function setType(string $type = 'SPOT'): self
-    {
-        $this->type = $type;
+	public function getType(): ?string {
+		return $this->type;
+	}
 
-        return $this;
-    }
+	/**
+	 * Specify transaction type (Spot, Futures, Margin, etc.) Type is different between exchanges and currency.
+	 *
+	 * @param string $type (default = "SPOT" for 0.0.1-pre-alpha)
+	 *
+	 * @return $this
+	 */
+	public function setType( string $type = 'SPOT' ): self {
+		$this->type = $type;
 
-    public function getIdCurrency(): ?Currency
-    {
-        return $this->idCurrency;
-    }
+		return $this;
+	}
 
-    public function setIdCurrency(?Currency $idCurrency): self
-    {
-        $this->idCurrency = $idCurrency;
+	public function getIdCurrency(): ?Currency {
+		return $this->idCurrency;
+	}
 
-        return $this;
-    }
+	public function setIdCurrency( ?Currency $idCurrency ): self {
+		$this->idCurrency = $idCurrency;
 
-    public function getOrderDirection(): string
-    {
-        return $this->orderDirection;
-    }
+		return $this;
+	}
 
-    /**
-     * Can be "BUY", "SELL" or "TRANSFERT".
-     *
-     * @return $this
-     */
-    public function setOrderDirection(string $orderDirection = 'BUY'): self
-    {
-        $this->orderDirection = $orderDirection;
+	public function getOrderDirection(): string {
+		return $this->orderDirection;
+	}
 
-        return $this;
-    }
+	/**
+	 * Can be "BUY", "SELL" or "TRANSFERT".
+	 *
+	 * @return $this
+	 */
+	public function setOrderDirection( string $orderDirection = 'BUY' ): self {
+		$this->orderDirection = $orderDirection;
 
-    public function getAmount(): ?int
-    {
-        return $this->amount;
-    }
+		return $this;
+	}
 
-    public function setAmount(int $amount): self
-    {
-        $this->amount = $amount;
+	public function getAmount(): ?int {
+		return $this->amount;
+	}
 
-        return $this;
-    }
+	public function setAmount( int $amount ): self {
+		$this->amount = $amount;
+
+		return $this;
+	}
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare( strict_types=1 );
 
 /*
  * This file is part of the Moneyes API project.
@@ -32,250 +32,223 @@ use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Validator\Constraints as Assert;
 
-#[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Table(name: '`user`')]
+#[ORM\Entity( repositoryClass: UserRepository::class )]
+#[ORM\Table( name: '`user`' )]
 #[ApiResource(
-    operations: [
-                 new GetCollection(normalizationContext: ['groups' => ['get:users']]),
-                 new Post(processor: UserPasswordHasher::class),
-                 new Get(normalizationContext: ['groups' => ['get:users', 'get:user', 'get:transactions']]),
-                new Put(processor: UserPasswordHasher::class),
-                 new Patch(processor: UserPasswordHasher::class),
-                 new Delete(),
-             ],
-    normalizationContext: ['groups' => ['user:read']],
-    denormalizationContext: ['groups' => ['user:create', 'user:update']],
+	operations: [
+		new GetCollection( normalizationContext: [ 'groups' => [ 'get:users' ] ] ),
+		new Get( normalizationContext: [ 'groups' => [ 'get:users', 'get:user', 'get:transactions' ] ] ),
+		new Post( processor: UserPasswordHasher::class ),
+		new Put( processor: UserPasswordHasher::class ),
+		new Patch( processor: UserPasswordHasher::class ),
+		new Delete(),
+	],
+	normalizationContext: [ 'groups' => [ 'user:read' ] ],
+	denormalizationContext: [ 'groups' => [ 'user:create', 'user:update' ] ],
 )]
-class User implements UserInterface, PasswordAuthenticatedUserInterface
-{
-    #[ORM\Id]
-    #[ORM\Column(type: UuidType::NAME, unique: true)]
-    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
-    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
-    #[Groups(['get:users'])]
-    private Uuid $id;
+class User implements UserInterface, PasswordAuthenticatedUserInterface {
+	#[ORM\Id]
+	#[ORM\Column( type: UuidType::NAME, unique: true )]
+	#[ORM\GeneratedValue( strategy: 'CUSTOM' )]
+	#[ORM\CustomIdGenerator( class: UuidGenerator::class )]
+	#[Groups( [ 'get:users' ] )]
+	private Uuid $id;
 
-    #[ORM\Column(length: 180, unique: true)]
-    #[Assert\NotBlank(groups: ['user:create'])]
-    #[Groups(['user:read', 'user:create', 'user:update'])]
-    private string $email;
+	#[ORM\Column( length: 180, unique: true )]
+	#[Assert\NotBlank( groups: [ 'user:create' ] )]
+	#[Groups( [ 'user:read', 'user:create', 'user:update' ] )]
+	private string $email;
 
-    #[ORM\Column]
-    #[Assert\NotBlank(groups: ['user:create'])]
-    #[Groups(['user:create', 'user:update'])]
-    private array $roles = [];
+	#[ORM\Column]
+	#[Assert\NotBlank( groups: [ 'user:create' ] )]
+	#[Groups( [ 'user:create', 'user:update' ] )]
+	private array $roles = [];
 
-    /** @var string The hashed password */
-    #[ORM\Column]
-    private string $password;
+	/** @var string The hashed password */
+	#[ORM\Column]
+	private string $password;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    #[Groups(['get:users', 'user:create', 'user:update'])]
-    private string $username;
+	#[ORM\Column( length: 255, nullable: true )]
+	#[Groups( [ 'get:users', 'user:create', 'user:update' ] )]
+	private string $username;
 
-    #[ORM\OneToMany(mappedBy: 'idUser', targetEntity: Transaction::class)]
-    #[Groups(['get:user', 'get:transactions'])]
-    private Collection $transactions;
+	#[ORM\OneToMany( mappedBy: 'idUser', targetEntity: Transaction::class )]
+	#[Groups( [ 'get:user', 'get:transactions' ] )]
+	private Collection $transactions;
 
-    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
-    #[Groups(['get:user', 'user:create', 'user:update'])]
-    private \DateTimeInterface $birthdate;
+	#[ORM\Column( type: Types::DATE_MUTABLE, nullable: true )]
+	#[Groups( [ 'get:user', 'user:create', 'user:update' ] )]
+	private \DateTimeInterface $birthdate;
 
-    #[ORM\Column(length: 255)]
-    #[Assert\NotBlank(groups: ['user:create'])]
-    #[Groups(['get:users', 'user:create', 'user:update'])]
-    private string $name;
+	#[ORM\Column( length: 255 )]
+	#[Assert\NotBlank( groups: [ 'user:create' ] )]
+	#[Groups( [ 'get:users', 'user:create', 'user:update' ] )]
+	private string $name;
 
-    #[ORM\Column(length: 255)]
-    #[Assert\NotBlank(groups: ['user:create'])]
-    #[Groups(['get:users', 'user:create', 'user:update'])]
-    private string $lastname;
+	#[ORM\Column( length: 255 )]
+	#[Assert\NotBlank( groups: [ 'user:create' ] )]
+	#[Groups( [ 'get:users', 'user:create', 'user:update' ] )]
+	private string $lastname;
 
-    #[ORM\OneToMany(mappedBy: 'idUser', targetEntity: Account::class)]
-    private Collection $account;
+	#[ORM\OneToMany( mappedBy: 'idUser', targetEntity: Account::class )]
+	private Collection $account;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    #[Assert\NotBlank(groups: ['user:create'])]
-    #[Groups(['user:create', 'user:update'])]
-    private ?string $plainPassword = null;
+	#[ORM\Column( length: 255, nullable: true )]
+	#[Assert\NotBlank( groups: [ 'user:create' ] )]
+	#[Groups( [ 'user:create', 'user:update' ] )]
+	private ?string $plainPassword = null;
 
-    public function __construct()
-    {
-        $this->transactions = new ArrayCollection();
-        $this->account      = new ArrayCollection();
-    }
+	public function __construct() {
+		$this->transactions = new ArrayCollection();
+		$this->account      = new ArrayCollection();
+	}
 
-    public function __toString(): string
-    {
-        return $this->name;
-    }
+	public function __toString(): string {
+		return $this->name;
+	}
 
-    public function getId(): Uuid
-    {
-        return $this->id;
-    }
+	public function getId(): Uuid {
+		return $this->id;
+	}
 
-    public function getEmail(): string
-    {
-        return $this->email;
-    }
+	public function getEmail(): string {
+		return $this->email;
+	}
 
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
+	public function setEmail( string $email ): self {
+		$this->email = $email;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
-     */
-    public function getUserIdentifier(): string
-    {
-        return (string) $this->email;
-    }
+	/**
+	 * A visual identifier that represents this user.
+	 *
+	 * @see UserInterface
+	 */
+	public function getUserIdentifier(): string {
+		return (string) $this->email;
+	}
 
-    /**
-     * @see UserInterface
-     */
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+	/**
+	 * @see UserInterface
+	 */
+	public function getRoles(): array {
+		$roles = $this->roles;
+		// guarantee every user at least has ROLE_USER
+		$roles[] = 'ROLE_USER';
 
-        return array_unique($roles);
-    }
+		return array_unique( $roles );
+	}
 
-    public function setRoles(array $roles): self
-    {
-        $this->roles = $roles;
+	public function setRoles( array $roles ): self {
+		$this->roles = $roles;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @see PasswordAuthenticatedUserInterface
-     */
-    public function getPassword(): string
-    {
-        return $this->password;
-    }
+	/**
+	 * @see PasswordAuthenticatedUserInterface
+	 */
+	public function getPassword(): string {
+		return $this->password;
+	}
 
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
+	public function setPassword( string $password ): self {
+		$this->password = $password;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @see UserInterface
-     */
-    public function eraseCredentials(): void
-    {
-        $this->plainPassword = null;
-    }
+	/**
+	 * @see UserInterface
+	 */
+	public function eraseCredentials(): void {
+		$this->plainPassword = null;
+	}
 
-    public function getUsername(): string
-    {
-        return $this->username;
-    }
+	public function getUsername(): string {
+		return $this->username;
+	}
 
-    public function setUsername(string $username): self
-    {
-        $this->username = $username;
+	public function setUsername( string $username ): self {
+		$this->username = $username;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getBirthdate(): \DateTimeInterface
-    {
-        return $this->birthdate;
-    }
+	public function getBirthdate(): \DateTimeInterface {
+		return $this->birthdate;
+	}
 
-    public function setBirthdate(\DateTimeInterface $birthdate): self
-    {
-        $this->birthdate = $birthdate;
+	public function setBirthdate( \DateTimeInterface $birthdate ): self {
+		$this->birthdate = $birthdate;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @return Collection<int, Transaction>
-     */
-    public function getTransactions(): Collection
-    {
-        return $this->transactions;
-    }
+	/**
+	 * @return Collection<int, Transaction>
+	 */
+	public function getTransactions(): Collection {
+		return $this->transactions;
+	}
 
-    public function addTransaction(Transaction $transaction): self
-    {
-        if (!$this->transactions->contains($transaction)) {
-            $this->transactions->add($transaction);
-            $transaction->setIdUser($this);
-        }
+	public function addTransaction( Transaction $transaction ): self {
+		if ( ! $this->transactions->contains( $transaction ) ) {
+			$this->transactions->add( $transaction );
+			$transaction->setIdUser( $this );
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function removeTransaction(Transaction $transaction): self
-    {
-        // if ($this->transactions->removeElement($transaction)) {
-        // TODO : Supprimer une transaction
-        // }
+	public function removeTransaction( Transaction $transaction ): self {
+		// if ($this->transactions->removeElement($transaction)) {
+		// TODO : Supprimer une transaction
+		// }
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getName(): string
-    {
-        return $this->name;
-    }
+	public function getName(): string {
+		return $this->name;
+	}
 
-    public function setName(string $name): self
-    {
-        $this->name = $name;
+	public function setName( string $name ): self {
+		$this->name = $name;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getLastname(): string
-    {
-        return $this->lastname;
-    }
+	public function getLastname(): string {
+		return $this->lastname;
+	}
 
-    public function setLastname(string $lastname): self
-    {
-        $this->lastname = $lastname;
+	public function setLastname( string $lastname ): self {
+		$this->lastname = $lastname;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @return Collection<int, Account>
-     */
-    public function getAccount(): Collection
-    {
-        return $this->account;
-    }
+	/**
+	 * @return Collection<int, Account>
+	 */
+	public function getAccount(): Collection {
+		return $this->account;
+	}
 
-    public static function createFromPayload(string $username, array $payload): void
-    {
-        // TODO: Implement createFromPayload() method.
-    }
+	public static function createFromPayload( string $username, array $payload ): void {
+		// TODO: Implement createFromPayload() method.
+	}
 
-    public function getPlainPassword(): ?string
-    {
-        return $this->plainPassword;
-    }
+	public function getPlainPassword(): ?string {
+		return $this->plainPassword;
+	}
 
-    public function setPlainPassword(string $plainPassword): self
-    {
-        $this->plainPassword = $plainPassword;
+	public function setPlainPassword( string $plainPassword ): self {
+		$this->plainPassword = $plainPassword;
 
-        return $this;
-    }
+		return $this;
+	}
 }
