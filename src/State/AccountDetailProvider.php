1b80006ab342e03b12942b8dc136f150f2f1cb2a
toboutal <toboutal@gmail.com>
2023-02-01 17:03:40 +0100

    add files

---
+++ src/State/AccountDetailProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+use App\Entity\Account;
+use App\Entity\Holding;
+use Doctrine\ORM\EntityManagerInterface;
+
+class AccountDetailProvider implements ProviderInterface
+{
+    public function __construct(private readonly EntityManagerInterface $manager)
+    {
+    }
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
+    {
+        $jsonReturn = [
+            'holdings'     => [],
+            'accountLabel'     => "",
+        ];
+
+        $account = $this->manager->getRepository(Account::class)->find($uriVariables['id']);
+        if (!$account instanceof Account) {
+            return ["Le compte n'existe pas"];
+        }
+        $jsonReturn["accountLabel"] = $account->getExchange()->getLabel();
+        $holdings                = $this->manager->getRepository(Holding::class)->findBy(['account' => $account]);
+        foreach ($holdings as $index => $holding) {
+            $jsonReturn["holdings"][$index] = array(
+                "currency" => $holding->getCurrency(),
+                "quantity" => $holding->getQuantity(),
+                "averagePurchasePrice" => $holding->getAveragePurchasePrice(),
+            );
+        }
+        return $jsonReturn;
+        // Retrieve the state from somewhere
+    }
+}
