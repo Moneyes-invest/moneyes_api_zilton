abe7816cb6d3bb39ba7a06122a3dbaee10242943
Vincent ASTA <vincent.asta1@gmail.com>
2023-04-05 16:40:57 +0200

    fix: pre-commit

--- symfony/src/Entity/Account.php
+++ symfony/src/Entity/Account.php
@@ -11,8 +11,6 @@ declare(strict_types = 1);
 
 namespace App\Entity;
 
-use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
-use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
